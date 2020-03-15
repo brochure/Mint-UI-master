2ff63ce584dbca8b485ebdbab242f6e0fdf5c285
Robert <zljlwty@qq.com>
2020-03-15 19:38:52 +0800

    add vue-resource pack
    for http service

--- src/main.js
+++ src/main.js
@@ -4,13 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Mint from 'mint-ui'
-import 'mint-ui/lib/style.css'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios';
+import VueResource from 'vue-resource';
+import 'mint-ui/lib/style.css'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.prototype.$axios=axios;
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(VueResource)
 
 // 引入全部组件 
 
