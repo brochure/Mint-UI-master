c996a2b94b96ce69739c0236b95aad03dbf4fb33
ashyash <zljlwty@qq.com>
2020-03-16 17:33:52 +0800

    restruct main page and icons

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,35 +1,45 @@
 <template>
   <div id="tab-bar">
-    <mt-tabbar v-model="selected" v-show="isShow" fixed class="border-1px-top">
-      <mt-tab-item id="main">
-        <mt-cell icon='eleme' class="footicon"></mt-cell>
-        外卖
-      <!--  <img :src="img1" slot="icon">外卖-->
-      </mt-tab-item>
-      <mt-tab-item id="life">
-        <mt-cell icon='life' class="footicon"></mt-cell>
-        生活
-      </mt-tab-item>
+    <el-footer>
+      <mt-tabbar v-model="selected" v-show="isShow" fixed style="border:none;">
+        <mt-tab-item id="main">
+          <!-- <mt-cell icon='eleme' class="footicon"></mt-cell> -->
+          <i class="el-icon-eleme" style="font-size:2em;"/>
+          <p class="foottxt">外卖</p>
+        <!--  <img :src="img1" slot="icon">外卖-->
+        </mt-tab-item>
+        <mt-tab-item id="life">
+          <!-- <mt-cell icon='life' class="footicon"></mt-cell> -->
+          <i class="mintui mintui-life" style="font-size:2em;"/>
+          <p class="foottxt">生活</p>
+        </mt-tab-item>
 
-      <mt-tab-item id="order">
-        <mt-cell icon='order' class="footicon"></mt-cell>
-        订单
-      </mt-tab-item>
+        <mt-tab-item id="order">
+          <!-- <mt-cell icon='order' class="footicon"></mt-cell> -->
+          <i class="mintui mintui-order" style="font-size:2em;"/>
+          <p class="foottxt">订单</p>
+        </mt-tab-item>
 
-      <mt-tab-item id="info">
-        <mt-cell icon='info' class="footicon"></mt-cell>
-        我的
-      </mt-tab-item>
-    </mt-tabbar>
+        <mt-tab-item id="info">
+          <i class="mintui mintui-info" style="font-size:2em;"/>
+          <!-- <mt-cell icon='info' class="footicon"></mt-cell> -->
+          <p class="foottxt">我的</p>
+        </mt-tab-item>
+      </mt-tabbar>
+    </el-footer>
   </div>
 </template>
 <style>
 @import url("//at.alicdn.com/t/font_1684162_56otp1zo8mb.css");
-.footicon{
-margin-bottom:0px;
-background:none;
-margin:-15px 0px -12px 0px;
+.foottxt{
+  margin-top:5px;
+  margin-bottom:2px;
 }
+/* .footicon{
+  background:none;
+  border: none;
+  margin:-15px 0px -12px 0px;
+} */
 </style>
 <script>
   export default {
