1306c27ca9012e38dce31804e9b58ad8707cc692
ashyash <zljlwty@qq.com>
2020-03-16 17:12:23 +0800

    switch frame of main page from bootstrap to el ui

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,119 +1,120 @@
 <template>
   <div id="index">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="pos-f-t col-12">
-        <div class="collapse" id="navbarToggleExternalContent">
-          <div class="bg-light p-4">
-            <h5 class="text-black h4">Collapsed content</h5>
-            <span class="text-muted">Toggleable via the navbar brand.</span>
+    <el-container>
+      <el-header>
+        <el-row style="padding-top:10px;">
+          <el-dropdown>
+            <span class="el-dropdown-link" style="font-size:1.2em;">
+              <i class="el-icon-location"/>
+              航科大厦（源深路）<i class="el-icon-arrow-down el-icon--right"/>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>当前定位</el-dropdown-item>
+              <el-dropdown-item>当前定位</el-dropdown-item>
+              <el-dropdown-item divided>收货地址</el-dropdown-item>
+              <el-dropdown-item>收货地址</el-dropdown-item>
+              <el-dropdown-item divided>附近地址</el-dropdown-item>
+              <el-dropdown-item>附近地址</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-row>
+        <el-row style="margin-top:10px;">
+          <div fixed style="height:60px;">
+            <mt-search
+              v-model="searchContent"
+              cancel-text="取消"
+              placeholder="我爱专星送"
+              style="width:100%;">
+            </mt-search>
           </div>
-        </div>
-        <nav class="navbar navbar-light bg-light">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <!-- <i class="el-icon-location"> -->
-            <span>航科大厦</span>
-          </button>
-          <mt-button icon="scan"></mt-button>
-           <mt-button icon="message" @click="exportJSON"></mt-button>
-        </nav>
-      </div>
-    </div>
-        <div fixed style="height:60px;">
-          <mt-search
-            v-model="searchContent"
-            cancel-text="取消"
-            placeholder="我爱专星送"
-            style="width:100%;">
-          </mt-search>
-        </div>
-        
-    <div class="block">
-      <el-carousel trigger="click" height="150px">
-        <el-carousel-item v-for="item in banners" :key="item.pic">
-          <img :src="item.pic" class="banner"/>
-        </el-carousel-item>
-      </el-carousel>
-    </div>
+        </el-row>
+      </el-header>
+      <el-main>
+        <!-- <div class="block"> -->
+        <el-row style="margin-top:20px;">
+          <el-carousel trigger="click" height="150px">
+            <el-carousel-item v-for="item in banners" :key="item.pic">
+              <img :src="item.pic" class="banner"/>
+            </el-carousel-item>
+          </el-carousel>
+        </el-row>
 
-    <div class="container my-2 mb-3">
-      <div class="row">
-        <div class="col-xs-1-5">
-        <div class="cpanel qsort">
-            <div class="icon">
-                <span>美食</span>
+        <el-row type="flex" justify="space-around">
+          <el-col :span="4">
+            <div class="cpanel qsort">
+                <div class="icon">
+                    <span>美食</span>
+                </div>
             </div>
-        </div>
-        </div>
-        <div class="col-xs-1-5">
-          <div class="cpanel qsort">
-            <div class="icon">
-        <span>商超便利</span>
+          </el-col>
+            <el-col :span="4">
+              <div class="cpanel qsort">
+                <div class="icon">
+            <span>商超便利</span>
+                </div>
             </div>
-        </div>
-        </div>
-        <div class="col-xs-1-5">
-          <div class="cpanel qsort">
-            <div class="icon">
-                <span>买菜</span>
+            </el-col>
+              <el-col :span="4">
+              <div class="cpanel qsort">
+                <div class="icon">
+                    <span>买菜</span>
+                </div>
             </div>
-        </div>
-        </div>
-        <div class="col-xs-1-5">
-          <div class="cpanel qsort">
-            <div class="icon">
-                <span>跑腿代购</span>
+              </el-col>
+              <el-col :span="4">
+              <div class="cpanel qsort">
+                <div class="icon">
+                    <span>跑腿代购</span>
+                </div>
+              </div>
+              </el-col>
+              <el-col :span="4">
+              <div class="cpanel qsort">
+                <div class="icon">
+                    <span>送药上门</span>
+                </div>
+            </div>
+              </el-col>
+        </el-row>
+
+        <!-- <div class="container"> -->
+        <el-row style="margin-top:20px;">
+          <div class="card-group bg-warning rounded-lg p-1" style="height:216px;">
+            <div class="col-12 ml-2" style="margin-bottom:-10px">
+            <p class="text-danger d-inline-block">新人专属</p><p class="text-danger d-inline-block font-weight-bold hight-amt">1</p><p class="text-danger d-inline-block">元起</p>
+            <p class="small font-weight-light" style="float:right;margin-top:2px;">更多 ></p>
+            </div>
+            <div class="card col-4 p-0 d-inline-block" v-for="value in discounts" :title="value.title" :key="value.title">
+              <img :src="value.pic" class="card-img-top discountimg">
+                <div class="card-body p-2">
+                  <p class="card-title text-nowrap small m-1">{{value.title}}</p>
+                  <p class="card-text text-muted mt-1"><del class="small">¥{{value.origin}}</del><a href="#" class="badge badge-info mt-1" style="float:right;">1元抢</a></p>
+                </div>
             </div>
           </div>
-        </div>
-        <div class="col-xs-1-5">
-          <div class="cpanel qsort">
-            <div class="icon">
-                <span>送药上门</span>
-            </div>
-        </div>
-        </div>
-        </div>
-    </div>
+        </el-row>
+        <!-- </div> -->
 
-<div class="container">
-    <div class="card-group bg-warning rounded-lg p-1" style="height:216px;">
-      <div class="col-12 ml-2" style="margin-bottom:-10px">
-      <p class="text-danger d-inline-block">新人专属</p><p class="text-danger d-inline-block font-weight-bold hight-amt">1</p><p class="text-danger d-inline-block">元起</p>
-      <p class="small font-weight-light" style="float:right;margin-top:2px;">更多 ></p>
-      </div>
-      <div class="card col-4 p-0 d-inline-block" v-for="value in discounts" :title="value.title" :key="value.title">
-        <img :src="value.pic" class="card-img-top discountimg">
-          <div class="card-body p-2">
-            <p class="card-title text-nowrap small m-1">{{value.title}}</p>
-            <p class="card-text text-muted mt-1"><del class="small">¥{{value.origin}}</del><a href="#" class="badge badge-info mt-1" style="float:right;">1元抢</a></p>
-          </div>
-      </div>
-    </div>
-</div>
-
-
-<mt-navbar v-model="selected">
-  <mt-tab-item id="1">推荐</mt-tab-item>
-  <mt-tab-item id="2">果蔬商超</mt-tab-item>
-  <mt-tab-item id="3">到店自取</mt-tab-item>
-</mt-navbar>
-
-<mt-tab-container v-model="selected">
-  <mt-tab-container-item id="1">
-    <merchants-list :filters="filters1"></merchants-list>
-  </mt-tab-container-item>
-  <mt-tab-container-item id="2">
-    <merchants-list :filters="filters2"></merchants-list>
-  </mt-tab-container-item>
-  <mt-tab-container-item id="3">
-    <merchants-list :filters="filters3"></merchants-list>
-  </mt-tab-container-item>
-</mt-tab-container>
-
-  </div>
-    
-
+      <el-row>
+        <mt-navbar v-model="selected">
+          <mt-tab-item id="1">推荐</mt-tab-item>
+          <mt-tab-item id="2">果蔬商超</mt-tab-item>
+          <mt-tab-item id="3">到店自取</mt-tab-item>
+        </mt-navbar>
+      </el-row>
+        <mt-tab-container v-model="selected">
+          <mt-tab-container-item id="1">
+            <merchants-list :filters="filters1"></merchants-list>
+          </mt-tab-container-item>
+          <mt-tab-container-item id="2">
+            <merchants-list :filters="filters2"></merchants-list>
+          </mt-tab-container-item>
+          <mt-tab-container-item id="3">
+            <merchants-list :filters="filters3"></merchants-list>
+          </mt-tab-container-item>
+        </mt-tab-container>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -214,45 +215,11 @@
   padding:10px;
 }
 
-.col-xs-1-5,
-		.col-sm-1-5,
-		.col-md-1-5,
-		.col-lg-1-5 {
-			position: relative;
-			min-height: 1px;
-			padding-right: 10px;
-			padding-left: 10px;
-		}
- 
-		.col-xs-1-5 {
-			width: 20%;
-			float: left;
-		}
- 
-		@media (min-width: 768px) {
-		.col-sm-1-5 {
-				width: 20%;
-				float: left;
-			}
-		}
-		@media (min-width: 992px) {
-			.col-md-1-5 {
-				width: 20%;
-				float: left;
-			}
-		}
-		@media (min-width: 1200px) {
-			.col-lg-1-5 {
-				width: 20%;
-				float: left;
-			}
-		}
-
-    .hight-amt{
-      font-size:1.3em;
-      margin-left: 3px;
-      margin-right: 3px;
-    }
+  .hight-amt{
+    font-size:1.3em;
+    margin-left: 3px;
+    margin-right: 3px;
+  }
 </style>
 
 <script>
